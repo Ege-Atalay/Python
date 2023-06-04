88be427b3f90434898d7709a540eb2a5bbd32e9c
Ege-Atalay <134312256+Ege-Atalay@users.noreply.github.com>
2023-06-04 18:28:53 +0300

    Create şifre_ve_kullanıcı_adı.py

---
+++ if_else_elif_komutları/şifre_ve_kullanıcı_adı.py
@@ -0,0 +1,29 @@
+#lütfen önce kendiniz yapın
+kadi = "ege"
+sifre= "atalay"
+
+deneme = 3
+
+while True:
+    ad_girisi = input("Lütfen Sisteme Kayıtlı Kullanıcı Adınızı Giriniz: ")
+    sifre_girisi = input("Lütfen Sisteme Kayıtlı Şifrenizi Giriniz: ")
+    
+    if (ad_girisi != kadi and sifre_girisi != sifre):
+        print("Sisteme Kayıtlı Kullanıcı Adı ve Parola Hatalıdır..!")
+        deneme -= 1
+
+    elif (ad_girisi != kadi and sifre_girisi == sifre):
+        print("Sisteme Kayıtlı Kullanıcı Adı ve Parola Hatalıdır..!")
+        deneme -= 1    
+        
+    elif (ad_girisi == kadi and sifre_girisi != sifre):
+        print("Sisteme Kayıtlı Kullanıcı Adı ve Parola Hatalıdır..!")
+        deneme -= 1 
+        
+    else:
+        print("Sisteme Hoşgeldiniz..!")
+        break
+        
+    if (deneme == 0):
+        print("Kullanıcı Adınız Bloke Olmuştur. Lütfen Sistem Yöneticiniz ile Görüşünüz..!")
+        break
